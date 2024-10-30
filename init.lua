d12c9169cbd5e24b0b4e411b3993f1037aa1d25b
MungShu <mungshushen@gmail.com>
2024-10-29 22:16:39 -0400

    init

--- init.lua
+++ init.lua
@@ -874,17 +874,15 @@ require('lazy').setup({
         send_motion = '+',
         send_file = '+F',
       },
-
       config = {
         -- This defines how the repl is opened. Here, we set the REPL window
         -- to open in a horizontal split to the bottom, with a height of 10.
-        repl_open_cmd = 'horizontal bot 10 split',
+        repl_open_cmd = 'vertical 60% split',
         -- This defines which binary to use for the REPL. If `ipython` is
         -- available, it will use `ipython`, otherwise it will use `python3`.
         -- since the python repl does not play well with indents, it's
         -- preferable to use `ipython` or `bypython` here.
         -- (see: https://github.com/Vigemus/iron.nvim/issues/348)
-        filetype = 'python',
         repl_definition = {
           python = {
             command = function()
@@ -896,6 +894,111 @@ require('lazy').setup({
         },
       },
     },
+  },
+  {
+    'Pocco81/auto-save.nvim',
+    keys = {
+      { '<leader>n', ':ASToggle<CR>', {}, desc = '🪴 Toggle AutoSave' },
+    },
+    config = function()
+      require('auto-save').setup {
+        -- your config goes herem
+        -- or just leave it empty :)
+      }
+    end,
+  },
+  {
+    'ThePrimeagen/harpoon',
+    branch = 'harpoon2',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    config = function()
+      local harpoon = require 'harpoon'
+      --- REQUIRED
+      harpoon:setup()
+      --- REQUIRED
+      -- local conf = require('telescope.config').values
+      -- local function toggle_telescope(harpoon_files)
+      --   local file_paths = {}
+      --   for _, item in ipairs(harpoon_files.items) do
+      --     table.insert(file_paths, item.value)
+      --   end
+      --
+      --   require('telescope.pickers')
+      --     .new({}, {
+      --       prompt_title = 'Harpoon',
+      --       finder = require('telescope.finders').new_table {
+      --         results = file_paths,
+      --       },
+      --       previewer = conf.file_previewer {},
+      --       sorter = conf.generic_sorter {},
+      --     })
+      --     :find()
+      -- end
+      -- vim.keymap.set('n', '<C-e>', function()
+      --   toggle_telescope(harpoon:list())
+      -- end, { desc = 'Open harpoon window' })
+      vim.keymap.set('n', '<leader>a', function()
+        harpoon:list():add()
+      end)
+      vim.keymap.set('n', '<C-e>', function()
+        harpoon.ui:toggle_quick_menu(harpoon:list())
+      end)
+
+      vim.keymap.set('n', '<M-1>', function()
+        harpoon:list():select(1)
+      end)
+      vim.keymap.set('n', '<M-2>', function()
+        harpoon:list():select(2)
+      end)
+      vim.keymap.set('n', '<M-3>', function()
+        harpoon:list():select(3)
+      end)
+
+      vim.keymap.set('n', '<M-4>', function()
+        harpoon:list():select(4)
+      end)
+
+      vim.keymap.set('t', '<M-e>', function()
+        harpoon.ui:toggle_quick_menu(harpoon:list())
+      end)
+
+      -- Toggle previous & next buffers stored within Harpoon list
+      vim.keymap.set('n', '<M-k>', function()
+        harpoon:list():prev()
+      end)
+      vim.keymap.set('n', '<M-j>', function()
+        harpoon:list():next()
+      end)
+
+      -- [[
+      --    --- Telescope UI
+      --    local harpoon = require 'harpoon'
+      --    harpoon:setup {}
+      --
+      --    -- basic telescope configuration
+      --    local conf = require('telescope.config').values
+      --    local function toggle_telescope(harpoon_files)
+      --      local file_paths = {}
+      --      for _, item in ipairs(harpoon_files.items) do
+      --        table.insert(file_paths, item.value)
+      --      end
+      --
+      --      require('telescope.pickers')
+      --        .new({}, {
+      --          prompt_title = 'Harpoon',
+      --          finder = require('telescope.finders').new_table {
+      --            results = file_paths,
+      --          },
+      --          previewer = conf.file_previewer {},
+      --          sorter = conf.generic_sorter {},
+      --        })
+      --        :find()
+      --    end
+      --
+      --    vim.keymap.set('n', '<C-e>', function()
+      --      toggle_telescope(harpoon:list())
+      --    end, { desc = 'Open harpoon window' })
+    end,
   },
   { -- You can easily change to a different colorscheme.
     -- Change the name of the colorscheme plugin below, and then
